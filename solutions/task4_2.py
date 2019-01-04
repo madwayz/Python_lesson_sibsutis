a121d6506822ac2951dfae3fb392d3b215f23871
madwayz <djurball@list.ru>
2019-01-04 21:31:52 +0800

    Solved 4.2

--- solutions/task4_2.py
+++ solutions/task4_2.py
@@ -1,2 +1,20 @@
+from collections import defaultdict
+
 if __name__ == '__main__':
-    count = input('Введите кол-во слов: ')
+    lat_eng = defaultdict(list)
+    counter = int(input('Введите кол-во английских слов: '))
+    print(
+        'Заполните словарь.\n'
+        'Пример: apple - malum, pomum, popula\n'
+    )
+
+    for _ in range(counter):
+        eng_word, lat_translate_from_eng = input('Осталось ' + str(counter) + ' | Ответ: ').split(' - ')
+
+        for lat_word in lat_translate_from_eng.split(', '):
+            lat_eng[lat_word].append(eng_word)
+        counter -= 1
+
+    print(len(lat_eng))
+    for lat, eng_translate in sorted(lat_eng.items()):
+        print(lat + ' - ' + ', '.join(eng_translate))
