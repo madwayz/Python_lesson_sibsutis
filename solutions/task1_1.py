4d2af76dba288001e2da0038c26c2e79770c671a
madwayz <djurball@list.ru>
2019-01-04 17:33:07 +0800

    Refactoring

--- solutions/task1_1.py
+++ solutions/task1_1.py
@@ -1,16 +1,15 @@
 import sys
 
+
 if __name__ == "__main__":
-    arr = list()
-    for i in range(4):
-        arr.append(int(input('Введите число: ')))
-        print(str(i) + ' ' + str(arr[i]))
-        if(arr[i] < 1 or arr[i] > 8):
+    arr = list(map(int, input('Введите число: ').split()))
+    if len(arr) > 4:
+        print('Слишком много чисел')
+        sys.exit(0)
+
+    for element in arr:
+        if element < 1 or element > 8:
             print('Числа должны быть в диапазоне от 1 до 8')
             sys.exit(0)
 
-    for x in arr:
-        if((arr[0] + arr[1]) % 2 == (arr[2] + arr[3]) % 2):
-            print('True')
-        else: print('False')
-    print(arr)
+    print(sum(arr) % 2 == 0)
