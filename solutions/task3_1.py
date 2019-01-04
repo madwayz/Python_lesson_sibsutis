a987bb26eb73480e78333f04debaff3da51ba79b
madwayz <djurball@list.ru>
2019-01-04 17:54:14 +0800

    Refactoring

--- solutions/task3_1.py
+++ solutions/task3_1.py
@@ -1,5 +1,3 @@
 if __name__ == '__main__':
     arr = input('Введите числа, разделённые пробелом: ').split()
-    for x in range(0, len(arr)):
-        if((x % 2) == 0):
-            print(arr[x])
+    print(arr[::2])
