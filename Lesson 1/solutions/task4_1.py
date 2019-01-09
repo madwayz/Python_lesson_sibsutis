ed942de1d8664b25727402110ede9f8d8362ef78
madwayz <djurball@list.ru>
2019-01-09 17:13:31 +0800

    Constant to input()

--- Lesson 1/solutions/task4_1.py
+++ Lesson 1/solutions/task4_1.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     dic = {}
-    text = 'one two one tho three'
+    text = input('Введите текст: ')
     for key in text.split():
         dic[key] = dic.get(key, 0) + 1
     print(dic)
