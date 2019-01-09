ca27ea555a2ce2407122795dc156f79c73e988aa
madwayz <djurball@list.ru>
2019-01-09 17:17:40 +0800

    Deleted condition.

--- Lesson 1/solutions/task1_1.py
+++ Lesson 1/solutions/task1_1.py
@@ -1,15 +1,3 @@
-import sys
-
-
 if __name__ == "__main__":
     arr = list(map(int, input('Введите число: ').split()))
-    if len(arr) > 4:
-        print('Слишком много чисел')
-        sys.exit(0)
-
-    for element in arr:
-        if element < 1 or element > 8:
-            print('Числа должны быть в диапазоне от 1 до 8')
-            sys.exit(0)
-
     print(sum(arr) % 2 == 0)
